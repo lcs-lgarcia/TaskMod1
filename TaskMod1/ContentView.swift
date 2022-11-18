685d1a1668b1ac817bd71ec8ad263d94b977898a
Lucas Garcia <lgarciaalonso@lcs.on.ca>
2022-11-17 22:51:30 -0500

    Initial Commit

---
+++ TaskMod1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TaskMod1
+//
+//  Created by Lucas García on 17/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaskMod1/TaskMod1App.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMod1App.swift
+//  TaskMod1
+//
+//  Created by Lucas García on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskMod1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
