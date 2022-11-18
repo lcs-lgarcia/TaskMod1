67a871fe23cfbc9b24b2159ed2f9caab8b9aee90
Lucas Garcia <lgarciaalonso@lcs.on.ca>
2022-11-17 23:43:39 -0500

    Gray box format progress

--- TaskMod1/Wheather.swift
+++ TaskMod1/Wheather.swift
@@ -16,40 +16,50 @@ struct Wheather: View {
                     .ignoresSafeArea()
                 
                 VStack{
+                    Spacer(minLength: 150)
                     Text("Hamilton")
                         .foregroundColor(Color.white)
                         .font(.largeTitle)
                     Text("4°")
+                        .font(Font.system(size: 110, weight: .thin))
                         .foregroundColor(Color.white)
                     Text("Cloudy")
                         .foregroundColor(Color.white)
-                        .font(.title)
+                        .font(Font.system(size: 30, weight: .regular))
                     HStack{
                         Text("H:5°")
                             .foregroundColor(Color.white)
+                            .font(Font.system(size: 30, weight: .medium))
                         Text("L:1°")
                             .foregroundColor(Color.white)
+                            .font(Font.system(size: 30, weight: .medium))
+                    }
+                    Spacer()
+                    VStack{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(width: 320,height: 150)
+                                .foregroundColor(Color("rect"))
+                            VStack{
+                                Text("Possible Light Rain")
+                                    .foregroundColor(.white)
+                                Text("Chance of light rain in the next hour.")
+                                    .foregroundColor(Color("RectText"))
+                            }
+                        }
                     }
                     VStack{
                         ZStack{
-                            RoundedRectangle(cornerRadius: 30)
-                                .frame(width: 320,height: 100)
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(width: 320,height: 150)
                                 .foregroundColor(Color("rect"))
                             Text("Loading")
                         }
                     }
                     VStack{
                         ZStack{
-                            RoundedRectangle(cornerRadius: 30)
-                                .frame(width: 320,height: 200)
-                                .foregroundColor(Color("rect"))
-                            Text("Loading")
-                        }
-                    }
-                    VStack{
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 30)
-                                .frame(width: 320,height: 200)
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(width: 320,height: 150)
                                 .foregroundColor(Color("rect"))
                             Text("Loading")
                         }
